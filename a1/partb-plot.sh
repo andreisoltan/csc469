03b9b542218c2fe6bc9dc1ecc55238404c56f3fe
Soltan Andrei <g0soltan@cdf.toronto.edu>
2013-02-13 23:37:04 -0500

    Added th elogs to the Makefile to be included in the tar. Edited the latex file for a couple of typos.

--- a1/partb-plot.sh
+++ a1/partb-plot.sh
@@ -2,15 +2,11 @@ paste <(cat perlbench-b3185-11.log | grep scrabbl | awk 'NR {if($2 == 2 && $3 ==
 
 gnuplot << EOF
 reset
-#set term epslatex size 4, 3 color 8
+set term epslatex size 4, 3 color 8
 set output "partb-plot.tex"
 set xlabel "Environment size (bytes)"
 set ylabel "runtime(O2)/runtime(O3)"
-#set pointsize 5
-#set key on center center box
-#set style line 1 lw 5 lc rgb "#66ff66" pt 7 ps 1.5
-#set style line 2 lt 2 lw 1 lc rgb "#aaaaff" pt 3 ps 1
-plot "partb-plot.dat" using 1:2 title "Environment size impact"
+plot "partb-plot.dat" using 1:2 notitle
 
 EOF
 
