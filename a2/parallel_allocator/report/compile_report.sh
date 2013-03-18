f247d98ff5558aa07366e674a72e0ce12f68def8
Jon Prindiville <g2prindi@cdf.toronto.edu>
2013-03-17 23:24:02 -0400

    Frag/Scal tables

--- a2/parallel_allocator/report/compile_report.sh
+++ a2/parallel_allocator/report/compile_report.sh
@@ -14,6 +14,17 @@ pushd larson-greywolf
     ./graphtests.pl >| out
 popd
 
+## Generate tabular data from variants ##########
+pushd latest-variants/
+    for file in allout-frag-grouped.tab allout-scal-grouped.tab ; do
+        grep larson $file \
+        | awk '{print $1, $2}' \
+        | sed -e '/[0-9]/ s/ / \& /g' \
+            -e '/[0-9]/ s/$/ \\\\ \\hline/ ' \
+            -e 's/^ *$/\\hline/g' >| `basename $file .tab`.tex
+    done
+popd
+
 ## Compile latex doc ############################
 latex report.tex
 latex report.tex
